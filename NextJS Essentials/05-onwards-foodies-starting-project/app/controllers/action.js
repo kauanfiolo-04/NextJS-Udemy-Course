3e18a3857bc89b91a992e1d84b1c6bca1df116b2
kauanfiolo-04 <kauanfiolo@hotmail.com>
2024-07-29 17:56:32 -0300

    Adding redirect after saving meal

--- NextJS Essentials/05-onwards-foodies-starting-project/app/controllers/action.js
+++ NextJS Essentials/05-onwards-foodies-starting-project/app/controllers/action.js
@@ -1,17 +1,19 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
-  // Server action
-  export const shareMeal=async (formData)=>{
-    const meal = {
-      title: formData.get("title"),
-      summary: formData.get("summary"),
-      instructions: formData.get("instructions"),
-      image: formData.get("image"),
-      creator: formData.get("name"),
-      creator_email: formData.get("email")
-    }
-
-    await saveMeal(meal);
+// Server action
+export const shareMeal=async (formData)=>{
+  const meal = {
+    title: formData.get("title"),
+    summary: formData.get("summary"),
+    instructions: formData.get("instructions"),
+    image: formData.get("image"),
+    creator: formData.get("name"),
+    creator_email: formData.get("email")
   }
+
+  await saveMeal(meal);
+  redirect("/meals");
+}
