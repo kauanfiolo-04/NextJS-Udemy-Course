630d440cda07206bb2931a3b93237ecdb9c31987
kauanfiolo-04 <kauanfiolo@hotmail.com>
2024-07-29 14:38:13 -0300

    Commit bomba

---
+++ NextJS Essentials/05-onwards-foodies-starting-project/app/meals/[slug]/page.js
@@ -0,0 +1,5 @@
+  export default function MealsSlug({ params }){
+  return (
+    <h1>Meals: {params.slug}</h1>
+  );
+}
---
+++ NextJS Essentials/05-onwards-foodies-starting-project/app/meals/share/page.js
@@ -0,0 +1,5 @@
+export default function MealsShare(){
+  return (
+    <h1>MealsShare</h1>
+  );
+}
