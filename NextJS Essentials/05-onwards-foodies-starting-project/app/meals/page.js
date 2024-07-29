53d7806ef81ff6a110ca504734b8a83c21dd708d
kauanfiolo-04 <kauanfiolo@hotmail.com>
2024-07-29 15:48:14 -0300

    Creating a DB and passing async/await to server components

--- NextJS Essentials/05-onwards-foodies-starting-project/app/meals/page.js
+++ NextJS Essentials/05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,8 +1,11 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "../controllers/melas";
 
-export default function Meals() {
+export default async function Meals() {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +20,7 @@ export default function Meals() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]}/>
+        <MealsGrid meals={meals}/>
       </main>
     </>
   );
