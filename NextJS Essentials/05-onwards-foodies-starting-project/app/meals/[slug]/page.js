85c35d381c10e7403ef771efa399045a4558eac3
kauanfiolo-04 <kauanfiolo@hotmail.com>
2024-07-29 16:27:14 -0300

    load and display data using slugs via params

--- NextJS Essentials/05-onwards-foodies-starting-project/app/meals/[slug]/page.js
+++ NextJS Essentials/05-onwards-foodies-starting-project/app/meals/[slug]/page.js
@@ -1,5 +1,32 @@
-  export default function MealsSlug({ params }){
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/app/controllers/meals";
+  
+export default function MealsDetailsPage({ params }){
+
+  const meal = getMeal(params.slug);
+
+  if(meal.instructions){
+    meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+  }
+
   return (
-    <h1>Meals: {params.slug}</h1>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal?.image ?? ""} alt={meal?.title ?? "some meal alt"} fill/>
+        </div>
+        <div className={classes.hederText}>
+          <h1>{meal?.title ?? 'Some Meal name'}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal?.creator_email ?? "Some Email"}`}>Name</a>
+          </p>
+          <p className={classes.summary}>summary</p>
+        </div>
+      </header>
+      <main>
+        <p className={classes.instructions}  dangerouslySetInnerHTML={{__html:meal?.instructions ?? "some instructions..."}} />
+      </main>
+    </>
   );
 }
