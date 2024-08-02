215421d7ffd89f82ad385f6314d0ac457a225c94
kauanfiolo-04 <kauanfiolo@hotmail.com>
2024-08-02 13:28:12 -0300

    Using catch-all to filtering and using fallback to

--- Routing and Page Render/starting-project/app/archive/@archive/[[...filter]]/page.js
+++ Routing and Page Render/starting-project/app/archive/@archive/[[...filter]]/page.js
@@ -1,21 +1,51 @@
-import { getAvailableNewsYears } from "@/app/helpers/news";
+import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/app/helpers/news";
+import NewsList from "@/components/news-list/news-list";
 import Link from "next/link";
 
 const FilteredNewsPage=({ params })=>{
   const filter = params.filter;
 
-  const links = getAvailableNewsYears();
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+
+  if(selectedYear && !selectedMonth){
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear); 
+  }
+
+  if(selectedYear && selectedMonth){
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No news found for the selected period.</p>;
+
+  if(news && news.length){
+    newsContent = <NewsList news={news} />;
+  }
 
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {links.map(link => <li key={link}>
-            <Link href={`/archive/${link}`}>{link}</Link>
-          </li>)}          
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map(link => {
+              const href = selectedYear ? `/archive/${selectedYear}/${link}` : `/archive/${link}`
+              
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}          
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
   );
 };
 
