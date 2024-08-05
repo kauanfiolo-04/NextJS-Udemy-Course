08154f0dd1823f0f1a5fa02d6fdbf4616d9a505c
kauanfiolo-04 <kauanfiolo@hotmail.com>
2024-08-05 17:38:01 -0300

    Using middleware

---
+++ Routing and Page Render/starting-project/middleware.js
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export const middleware=(request)=>{
+  console.log(request);
+  
+  return NextResponse.next();
+};
+
+export const config={
+  matcher: "/news"
+};
