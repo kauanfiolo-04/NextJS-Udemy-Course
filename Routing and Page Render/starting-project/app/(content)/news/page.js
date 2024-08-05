26ee4d4a0926ba364611a7b5a88da8eee6b7176b
kauanfiolo-04 <kauanfiolo@hotmail.com>
2024-08-05 19:14:25 -0300

    Server side data fetching

--- Routing and Page Render/starting-project/app/(content)/news/page.js
+++ Routing and Page Render/starting-project/app/(content)/news/page.js
@@ -1,51 +1,19 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import NewsList from "@/components/news-list/news-list";
 
-const News=()=>{
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
-  const [news, setNews] = useState();
-
-  useEffect(()=>{
-    const fetchNews= async ()=>{
-      setIsLoading(true);
-      const response = await fetch("http://localhost:8080/news");
-      
-      if(!response.ok){
-        setError("Failed to fetch news.");
-        setIsLoading(false);
-      }
-
-      const news = await response.json();
-      setIsLoading(false);
-      setNews(news);
-    };
-
-    fetchNews();
+const News=async ()=>{
+  const response = await fetch("http://localhost:8080/news");
     
-  },[]);
-
-  if(isLoading){
-    return <p>Loading...</p>;
+  if(!response.ok){
+    throw new Error("Failed to fetch news.");
   }
 
-  if(error){
-    return <p>{error}</p>
-  }
-
-  let newsContent;
- 
-  if(news){
-    newsContent = <NewsList news={news} />;
-  }
+  const news = await response.json();
 
   return(
-    <div>
+    <>
       <h1>News</h1>
-      {newsContent}
-    </div>
+      <NewsList news={news} />
+    </>
   );
 };
 
