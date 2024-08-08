2a54f34b984dc5ea1b4bf6141dd69b3a5463cd5e
kauanfiolo-04 <kauanfiolo@hotmail.com>
2024-08-07 21:51:25 -0300

    Validating user input with the useFormStatus

--- 3 Data Mutation/app/new-post/page.js
+++ 3 Data Mutation/app/new-post/page.js
@@ -1,13 +1,35 @@
+'use client';
+
+import { useFormState } from "react-dom";
 import FormSubmit from '@/components/form-submit';
 import { storePost } from '@/lib/posts';
 import { redirect } from 'next/navigation';
 
 export default function NewPostPage() {
+
   async function createPost(formData) {
     "use server";
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
+
+    let errors = [];
+
+    if(!title || title.trim().length===0){
+      errors.push("Title is required.");
+    }
+
+    if(!content || content.trim().length===0){
+      errors.push("Content is required.");
+    }
+
+    if(!image){
+      errors.push("Image is required.");
+    }
+
+    if(errors.length){
+      return { errors };
+    }
 
     await storePost({
       imageUrl: '',
@@ -19,10 +41,12 @@ export default function NewPostPage() {
     redirect('/feed');
   }
 
+  const [state, formAction] = useFormState(createPost, {});
+
   return (
     <>
       <h1>Create a new post</h1>
-      <form action={createPost}>
+      <form action={formAction}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
           <input type="text" id="title" name="title" />
