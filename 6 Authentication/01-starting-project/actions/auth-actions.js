99c2dd7707a8b2569a94700a396b66b752de57de
kauanfiolo-04 <kauanfiolo@hotmail.com>
2024-10-14 17:27:34 -0300

    using hash to encrypt user password

---
+++ 6 Authentication/01-starting-project/actions/auth-actions.js
@@ -0,0 +1,26 @@
+'use server';
+
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/lib/user";
+
+export async function signup(prevState, formData) {
+  const email = formData.get('email');
+  const password = formData.get('password');
+
+  let errors = {};
+
+  if (!email.includes('@')) {
+    errors.email = 'Please enter a valid email address!';
+  }
+
+  if (password.trim().length < 8) {
+    errors.password = 'Password must be at least 8 characters long.';
+  }
+
+  if (Object.keys(errors).length) {
+    return {errors};
+  }
+
+  const hashedPassword = hashUserPassword(password);
+  createUser(email, hashedPassword);
+}
